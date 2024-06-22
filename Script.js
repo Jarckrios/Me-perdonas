a84fb34f39eab7501cd98e8bfb3d00d2d036c496
Jarckrios <jarckrios14@gmail.com>
2024-06-22 00:51:19 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 const messageBox = document.getElementById('message-box');
 const btnNo = document.getElementById('btn-no');
+const btnYes = document.getElementById('btn-yes'); // Referencia al botón "Sí"
 
 // Array de mensajes de disculpa
 const messages = [
@@ -26,4 +27,10 @@ btnNo.addEventListener('click', () => {
   if (currentIndex === messages.length) {
     currentIndex = 0;
   }
+});
+
+// Función para reiniciar el mensaje cuando se presiona el botón "Sí"
+btnYes.addEventListener('click', () => {
+  currentIndex = 0; // Reiniciar el índice al inicio del array
+  messageBox.innerHTML = `<p>Gracias por perdonarme.</p>`;
 });
